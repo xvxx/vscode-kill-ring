e6b5203386b8342ee4abf2ada5715295f0f8e7cb
dvkt <c@dvkt.io>
2018-08-13 14:58:24 -0700

    cool

---
+++ extension.js
@@ -0,0 +1,32 @@
+const vscode = require('vscode');
+
+let yanked = null;
+
+function activate(context) {
+    let ydisposable = vscode.commands.registerCommand('extension.yank', function () {
+        if (!yanked) return;
+
+        let e = vscode.window.activeTextEditor;
+
+        if (e) {
+            const {document} = e;
+            const edit = new vscode.WorkspaceEdit();
+            edit.insert(document.uri, e.selection.start, yanked);
+            return vscode.workspace.applyEdit(edit)
+        }
+    });
+
+
+    let kdisposable = vscode.commands.registerCommand('extension.kill', function () {
+        vscode.commands.executeCommand("cursorEndSelect").then(function () {
+            let e = vscode.window.activeTextEditor
+            yanked = e.document.getText(e.selection)
+            vscode.commands.executeCommand("deleteAllRight")
+        })
+    });
+
+    context.subscriptions.push(ydisposable);
+    context.subscriptions.push(kdisposable);
+}
+exports.activate = activate;
+
