146d98e56ab8ca1f81349bd1b74a0ec3c293c5e4
dvkt <c@dvkt.io>
2019-04-23 13:30:10 -0700

    cmd: show kill history

--- extension.js
+++ extension.js
@@ -1,21 +1,31 @@
 const vscode = require('vscode');
 
 let yanked = null;
+let yanks = [];
+
+function insert(txt) {
+    let e = vscode.window.activeTextEditor;
+    if (e) {
+        const { document } = e;
+        const edit = new vscode.WorkspaceEdit();
+        edit.insert(document.uri, e.selection.start, txt);
+        return vscode.workspace.applyEdit(edit)
+    }
+}
+
+async function showHistory() {
+    const result = await vscode.window.showQuickPick(yanks, {
+        placeHolder: 'Kill history',
+        onDidSelectItem: item => vscode.window.showInformationMessage(`Yank ${++i}: ${item}`)
+    });
+    insert(result);
+}
 
 function activate(context) {
     let ydisposable = vscode.commands.registerCommand('kill-ring.yank', function () {
         if (!yanked) return;
-
-        let e = vscode.window.activeTextEditor;
-
-        if (e) {
-            const { document } = e;
-            const edit = new vscode.WorkspaceEdit();
-            edit.insert(document.uri, e.selection.start, yanked);
-            return vscode.workspace.applyEdit(edit)
-        }
+        insert(yanked);
     });
-
 
     let kdisposable = vscode.commands.registerCommand('kill-ring.kill', function () {
         let e = vscode.window.activeTextEditor
@@ -23,11 +33,18 @@ function activate(context) {
         let line = e.document.lineAt(s.start.line)
         let r = new vscode.Range(s.start, line.range.end)
         yanked = e.document.getText(r).trim()
+        yanks.push(yanked)
         vscode.commands.executeCommand("deleteAllRight")
+    });
+
+    let hdisposable = vscode.commands.registerCommand('kill-ring.history', function () {
+        if (yanks.length == 0) return;
+        showHistory();
     });
 
     context.subscriptions.push(ydisposable);
     context.subscriptions.push(kdisposable);
+    context.subscriptions.push(hdisposable);
 }
 exports.activate = activate;
 
